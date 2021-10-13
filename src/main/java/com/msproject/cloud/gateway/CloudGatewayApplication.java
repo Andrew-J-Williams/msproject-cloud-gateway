bd6d0b21409de0f6623500bf9fdfea3205568022
Andrew Williams <1williamsaj@gmail.com>
2021-10-13 18:00:17 -0500

    Annotated as eureka client, added eureka registration info in yaml

--- src/main/java/com/msproject/cloud/gateway/CloudGatewayApplication.java
+++ src/main/java/com/msproject/cloud/gateway/CloudGatewayApplication.java
@@ -2,8 +2,10 @@ package com.msproject.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
