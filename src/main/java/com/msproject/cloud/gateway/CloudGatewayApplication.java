e5e89d3c5aceb4f77c0bc2fbf7c29dc63011038d
Andrew Williams <1williamsaj@gmail.com>
2021-10-14 12:52:15 -0500

    Removed hystrix dependencies from pom.xml, removed filter from yaml

--- src/main/java/com/msproject/cloud/gateway/CloudGatewayApplication.java
+++ src/main/java/com/msproject/cloud/gateway/CloudGatewayApplication.java
@@ -3,11 +3,9 @@ package com.msproject.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
