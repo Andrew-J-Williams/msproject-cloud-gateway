6492c65b1592b2bf2b673fe42e3838a097daf877
Andrew Williams <1williamsaj@gmail.com>
2021-10-13 19:02:28 -0500

    Created userServiceFallBackMethod

--- src/main/java/com/msproject/cloud/gateway/FallBackMethodController.java
+++ src/main/java/com/msproject/cloud/gateway/FallBackMethodController.java
@@ -1,5 +1,15 @@
 package com.msproject.cloud.gateway;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FallBackMethodController {
 
+	@GetMapping("/userServiceFallBack")
+	public String userServiceFallBackMethod() {
+		return "User Service is taking longer than expeceted" + 
+			   "Please try again later!";
+	}
+	
 }
