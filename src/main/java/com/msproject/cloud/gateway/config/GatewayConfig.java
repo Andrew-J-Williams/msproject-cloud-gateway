579f0bfee7d1ac87f1937db28586df52c6693f30
Andrew Williams <1williamsaj@gmail.com>
2021-10-15 12:37:32 -0500

    Updated the uris on the GatewayConfig for each route

--- src/main/java/com/msproject/cloud/gateway/config/GatewayConfig.java
+++ src/main/java/com/msproject/cloud/gateway/config/GatewayConfig.java
@@ -1,9 +1,18 @@
 package com.msproject.cloud.gateway.config;
 
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
 
 @Configuration
 public class GatewayConfig {
@@ -13,21 +22,30 @@ public class GatewayConfig {
 	    return builder.routes()
 	    		.route(p -> p
 	    				.path("/users/**")
-	    				.uri("http://localhost:9002")
+	    				.filters(f -> f.circuitBreaker(c -> c.setName("userCircuitBreaker").setFallbackUri("/userServiceFallBack")))
+	    				.uri("http://USER-SERVICE")
 	    			  )
 	    		.route(p -> p
 	    				.path("/users/")
-	    				.uri("http://localhost:9002")
+	    				.uri("http://USER-SERVICE")
 	    			  )
 	    		.route(p -> p
 	    				.path("/departments/**")
-	    				.uri("http://localhost:9001")
+	    				.filters(f -> f.circuitBreaker(c -> c.setName("departmentCircuitBreaker").setFallbackUri("/departmentServiceFallBack")))
+	    				.uri("http://DEPARTMENT-SERVICE")
 	    			  )
 	    		.route(p -> p
 	    				.path("/departments/")
-	    				.uri("http://localhost:9001")
+	    				.uri("http://DEPARTMENT-SERVICE")
 	    			  )
 	    		.build();
 	}
 	
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory->factory.configureDefault(id-> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom()
+						.timeoutDuration(Duration.ofSeconds(2)).build()).build());
+	}
 }
