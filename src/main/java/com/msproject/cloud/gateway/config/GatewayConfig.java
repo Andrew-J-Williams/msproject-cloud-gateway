fb267aee2c180a648319313758c1c9a90266103a
Andrew Williams <1williamsaj@gmail.com>
2021-10-15 10:45:50 -0500

    Added annotations and RouteLocator + RouteLocatorBuilder into the GatewayConfig file

--- src/main/java/com/msproject/cloud/gateway/config/GatewayConfig.java
+++ src/main/java/com/msproject/cloud/gateway/config/GatewayConfig.java
@@ -1,5 +1,16 @@
 package com.msproject.cloud.gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class GatewayConfig {
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	    return builder.routes().build();
+	}
+	
 }
